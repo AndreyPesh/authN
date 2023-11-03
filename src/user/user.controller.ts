eea1b11cce0e6a69ac4489d7ebe6e1b907c6abab
Andrei <coursestud@mail.ru>
2023-11-03 16:08:22 +0300

    feat: add user crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post()
+    save(@Body() dto: { email: string; password: string }) {
+        return this.userService.save(dto);
+    }
+
+    @Get(':idOrEmail')
+    findOneUser(@Param('idOrEmail') idOrEmail: string) {
+        return this.userService.findOne(idOrEmail);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id', ParseUUIDPipe) id: string) {
+        return this.userService.delete(id);
+    }
+}
