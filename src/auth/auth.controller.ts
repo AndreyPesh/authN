2b36b5cfae90fcc8338d52a526ea56b36ffdd959
Andrei <coursestud@mail.ru>
2023-11-04 14:22:33 +0300

    feat: add set refresh token in cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,24 @@
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Get,
+    HttpStatus,
+    Post,
+    Res,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDto, RegisterDto } from './dto';
 import { AuthService } from './auth.service';
+import { Tokens } from './interfaces';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+const REFRESH_TOKEN = 'refreshtoken';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService, private readonly configService: ConfigService) {}
 
     @Post('register')
     async register(@Body() dto: RegisterDto) {
@@ -16,14 +30,29 @@ export class AuthController {
     }
 
     @Post('login')
-    async login(@Body() dto: LoginDto) {
+    async login(@Body() dto: LoginDto, @Res() res: Response) {
         const tokens = await this.authService.login(dto);
         if (!tokens) {
             throw new BadRequestException('Cant login! Something went wrong!');
         }
+        this.setRefreshTokenToCookies(tokens, res);
         return { accessToken: tokens.accessToken };
     }
 
     @Get('refresh')
     refreshTokens() {}
+
+    private setRefreshTokenToCookies(tokens: Tokens, res: Response) {
+        if (!tokens) {
+            throw new UnauthorizedException();
+        }
+        res.cookie(REFRESH_TOKEN, tokens.refreshToken.token, {
+            httpOnly: true,
+            sameSite: 'lax',
+            expires: new Date(tokens.refreshToken.exp),
+            secure: this.configService.get('NODE_ENV', 'development') === 'production',
+            path: '/',
+        });
+        res.status(HttpStatus.CREATED).json(tokens);
+    }
 }
